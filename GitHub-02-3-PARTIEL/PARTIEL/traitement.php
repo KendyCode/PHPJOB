8ea18bc3d42245386dc42cfd47ce5df91a519c72
AntoninNo <noel.antoninn@gmail.com>
2025-02-12 08:35:30 +0100

    Update traitement.php

--- GitHub-02-3-PARTIEL/PARTIEL/traitement.php
+++ GitHub-02-3-PARTIEL/PARTIEL/traitement.php
@@ -9,9 +9,6 @@ new Developpeur(151,"Katsiev", "Amir", "amir@test.net", "0606060606",2750,
 $manager = array(new Manager(500, "RIPAULT", "Alexis", "alexis@mana.net",
 
 
-$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net", "0606060606",4000, "DSI"), new Manager(600,"OLIVEIRA", "Romain", "romain@manager.net", "0808080808",3000, "Chef de service")) ;
-
-
 foreach ($developpeur as $value) {
 	echo $value->afficher();
 }
